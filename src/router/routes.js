437716fc74e6215bfb35e625d745d24f87847cfb
M.K <mr5h4nkly@gmail.com>
2022-08-13 19:56:54 +0430

    Update

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,7 @@
 
 const routes = [
   {
-    path: '/Meety-vue/',
+    path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
